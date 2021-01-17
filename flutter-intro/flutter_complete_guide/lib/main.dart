90b1752b537bdf51f3a02f33a14b7c73426b08e6
peachyapas <peachyapas@gmail.com>
2021-01-17 16:16:53 +0700

    finished tutorial

--- flutter-intro/flutter_complete_guide/lib/main.dart
+++ flutter-intro/flutter_complete_guide/lib/main.dart
@@ -11,19 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'Welcome to Flutter',
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: Text('Welcome to Flutter'),
-      //   ),
-      //   body: Center(
-      //     child: Text('Hello World'),
-      //     // child: Text('wordPair.asPascalCase'), //Pascal case” (also known as “upper camel case”), means that each word in the string, including the first one, begins with an uppercase letter. So, “uppercamelcase” becomes “UpperCamelCase”.
-      //     child: RandomWords(), //now calls the RandomWords class
-      //   ),
-      // ),
-      title: 'Startup Name Generator',
-      home: RandomWords(),
+      title: 'Create and Save Word Pairs',
+      home: RandomWords(), //displays this @ home
     );
   }
 }
@@ -36,14 +25,60 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <
       WordPair>[]; //_suggestions list to the _RandomWordsState class for saving suggested word pairings
+  final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 18.0);
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        //add MaterialRoute page and its builder
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asCamelCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  } //add pushsaved to randomwordstate class
+
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair); //make sure word pairing ahsn't already been added to favourites
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.yellow : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
@@ -68,6 +103,7 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
       ),
       body: _buildSuggestions(),
     );
@@ -82,19 +118,5 @@ class _RandomWordsState extends State<RandomWords> {
   // It increments twice for every suggested word pairing: once for the ListTile,
   // and once for the Divider. This model allows the suggested list to continue growing as
   // the user scrolls.
-
-  // Widget buildSuggestions() {
-  //   return ListView.builder(
-  //       padding: EdgeInsets.all(16.0),
-  //       itemBuilder: /*1*/ (context, i) {
-  //         if (i.isOdd) return Divider(); /*2*/
-
-  //         final index = i ~/ 2; /*3 */
-  //         if (index >= _suggestions.length) {
-  //           _suggestions.addAll(generateWordPairs().take(10));
-  //         }
-  //         return _buildRow(_suggestions[index]);
-  //       });
-  // }
 
 }
